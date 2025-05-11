efad7542efff832107b610e1732a5f353fde9436
Martín Reyes <mreyes@merovingiandata.com>
2025-05-11 15:32:23 -0300

    Fix Env Vars

--- Frontend/entrypoint.sh
+++ Frontend/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Combina el host y puerto para generar la API URL
+BACKEND_URL="${BACKEND_HOST}:${BACKEND_PORT}"
+
+echo "Generando archivo env-config.js con BACKEND_URL=${BACKEND_URL}"
+
 cat <<EOF > /usr/share/nginx/html/assets/env-config.js
 window.env = {
-  apiUrl: "${API_URL}"
+  apiUrl: "${BACKEND_URL}"
 };
 EOF
 
