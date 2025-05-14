9ff7bfe1282f87f1fea587866db711ae562c1087
Martín Reyes <mreyes@merovingiandata.com>
2025-05-13 23:17:45 -0300

    Fix Dockerfile & entrypoint.sh

--- Frontend/entrypoint.sh
+++ Frontend/entrypoint.sh
@@ -5,7 +5,7 @@ BACKEND_URL="http://${BACKEND_HOST}:${BACKEND_PORT}"
 
 echo "Generando archivo env-config.js con BACKEND_URL=${BACKEND_URL}"
 
-cat <<EOF > /usr/share/nginx/html/frontend/assets/env-config.js
+cat <<EOF > /usr/share/nginx/html/assets/env-config.js
 window.env = {
   apiUrl: "${BACKEND_URL}"
 };
