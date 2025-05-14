a988c56ad05429549a9a989357bd7b1af058c73e
Martín Reyes <mreyes@merovingiandata.com>
2025-05-14 20:48:35 -0300

    Fix env vars

--- Frontend/entrypoint.sh
+++ Frontend/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo "[entrypoint.sh] Generando archivo env.js con BACKEND_HOST=${BACKEND_HOST} y BACKEND_PORT=${BACKEND_PORT}..."
+# Salir si alguna variable requerida no está definida
+: "${BACKEND_HOST:?La variable BACKEND_HOST no está definida}"
+: "${BACKEND_PORT:?La variable BACKEND_PORT no está definida}"
 
-# Valores por defecto (opcional)
-: "${BACKEND_HOST:=localhost}"
-: "${BACKEND_PORT:=3000}"
+echo "[entrypoint.sh] Contenido antes de reemplazar:"
+cat /usr/share/nginx/html/assets/env.template.js
+
+echo "[entrypoint.sh] Generando archivo env.js con BACKEND_HOST=${BACKEND_HOST} y BACKEND_PORT=${BACKEND_PORT}..."
 
 # Inyectar variables en el template y generar env.js
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
