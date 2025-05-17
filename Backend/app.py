469368b2e794b884f034e743f89ffdb7333fcb4b
Martín Reyes <mreyes@merovingiandata.com>
2025-05-17 18:52:14 -0300

    Change endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -12,14 +12,14 @@ load_dotenv()
 mariadbclient = MariaDBClient()
 lock_manager = ResourceLockManager()
 
-@app.route('/info')
+@app.route('/api/info')
 def get_info():
     if lock_manager.is_locked():
         return jsonify({"error": "La base de datos está siendo actualizada. Intenta nuevamente en unos minutos."}), 423
     data = mariadbclient.get_info()
     return jsonify(data), 200
 
-@app.route('/productos/')
+@app.route('/api/productos/')
 def get_productos():
     if lock_manager.is_locked():
         return jsonify({"error": "La base de datos está siendo actualizada. Intenta nuevamente en unos minutos."}), 423
